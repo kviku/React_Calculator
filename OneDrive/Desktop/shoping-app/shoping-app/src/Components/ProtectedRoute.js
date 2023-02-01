028799a938673f8049af78453787eecafb2da341
kviku <KVIKU98@GMAIL.COM>
2023-02-01 23:43:58 +0530

    shoping

--- OneDrive/Desktop/shoping-app/shoping-app/src/Components/ProtectedRoute.js
+++ OneDrive/Desktop/shoping-app/shoping-app/src/Components/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ isEmpty, children }) => {
     if (isEmpty) { 
-    return <Navigate to="/" replace />;
+    //return <Navigate to="/" replace />;
 }
 return children;
 };
